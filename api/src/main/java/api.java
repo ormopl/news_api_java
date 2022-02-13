c42f536be37a0139532d1e8e78eb54e0579f40cf
artur <arturstepczynski@gmail.com>
2022-02-13 19:25:21 +0100

    GUI with textfield and status label added.

--- api/src/main/java/api.java
+++ api/src/main/java/api.java
@@ -1,3 +1,7 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,14 +11,69 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import org.json.JSONArray;
 import org.json.JSONObject;
-import java.io.IOException;
-import java.io.InterruptedIOException;
 
 
-public class api {
+public class api implements ActionListener {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
+    JFrame frame;
+    JButton button;
+    JLabel label;
+    JPanel panel;
+    JTextArea textAera;
+    public int status = 0;
+    public int writerStatus = 0;
+    public String line;
 
+
+    public api() {
+        //configuring the GUI
+        frame = new JFrame();
+        button = new JButton("Download!");
+        button.setSize(new Dimension(10, 10));
+        button.addActionListener(this);
+        label = new JLabel("Click the button to download news...");
+        textAera = new JTextArea(5, 20);
+        textAera.setEditable(false);
+
+        panel = new JPanel();
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+        panel.add(textAera);
+
+        frame.add(panel, BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setTitle("News API Downloader");
+        frame.setSize(400, 600);
+        frame.pack();
+        frame.setVisible(true);
+
+    }
+
+    public static void main(String[] args) {
+        new api();
+    }
+
+
+    @Override //button and labels behaviour
+    public void actionPerformed (ActionEvent e){
+        try {
+            getAPI();
+            label.setText("Connection Status: " + status);
+            if (writerStatus == 1) {
+                label.setText("news.txt file created!");
+            }else{
+                label.setText("Nothing Created");
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch (InterruptedException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void getAPI() throws IOException, InterruptedException { //old part of code, writer status and textfield output added
         //basic configuration
         String API_KEY = "0f85faabd2084b8db9f708dd6270e532";
         String category = "business";
@@ -42,9 +101,11 @@ public class api {
                 HttpResponse.BodyHandlers.ofString());
         String content = response_string.body();
 
-        if (response.statusCode() == 200){
+        if (response.statusCode() == 200) {
+
             System.out.println("Connection status: " + response.statusCode() + " OK!");
-        }else {
+            status = response.statusCode();
+        } else {
             System.out.println("Connection status: " + response.statusCode() + ". Stopping.");
             System.exit(0);
         }
@@ -59,21 +120,21 @@ public class api {
         FileWriter news_fileWriter = new FileWriter("news.txt");
         System.out.println("news.txt file created!");
 
-        //loop for JSON values parsing and writing to to file
+        //loop for JSON values parsing and writing to file
 
-        for (int i = 0; i < articles_arr.length(); i++){
+        for (int i = 0; i < articles_arr.length(); i++) {
             var author = articles_arr.getJSONObject(i).get("author");
             var title = articles_arr.getJSONObject(i).get("title");
             var description = articles_arr.getJSONObject(i).get("description");
-            var line = (title + ":" + description + ":" + author);
+            line = (title + ":" + description + ":" + author);
+            textAera.append(line + "\n");
             news_fileWriter.write(line + "\n");
+
         }
 
         //closing the file
         news_fileWriter.close();
-
-        }
-
-
+        writerStatus = 1;
 
     }
+}
